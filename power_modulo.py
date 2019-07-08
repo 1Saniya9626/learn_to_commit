850f881e6981dbe241f837a16320249ecb24e3a2
Sania <sania@Salman-Mac-mini.local>
2019-07-08 15:17:57 +0530

    committing excercise program

---
+++ power_modulo.py
@@ -0,0 +1,16 @@
+a = 5
+b = 3
+c = "2"
+
+# using power operator
+print (f"power:")
+print (a ** b)
+
+# using modulo operator
+print (f"modulo is:")
+print (a % b)
+
+# using with string
+print (f"operation on string and int:")
+print (a ** c)
+print (b % c)
